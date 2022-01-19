5882bb3ab36e42e2419bfe942d7ca9655dca6dd8
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2022-01-19 21:26:00 +0530

    lambda to stop EC2

---
+++ lambda_stop.py
@@ -0,0 +1,15 @@
+### Author :- Dheeraj Choudhary
+
+import json
+import boto3
+
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+
+def lambda_handler(event, context):
+    instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']},{'Name': 'tag:Env','Values':['Dev']}])
+    for instance in instances:
+        id=instance.id
+        ec2.instances.filter(InstanceIds=[id]).stop()
+        print("Instance ID is stopped"+instance.id)
+
+    return "success"
