97f4f5ada584482f6401a2dc48e011623ae95aae
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2022-01-20 19:13:51 +0530

    Fetch Unused Volumes

---
+++ unused_volumes.py
@@ -0,0 +1,21 @@
+### Author:- Dheeraj Choudhary
+
+import boto3
+ec2 = boto3.client('ec2')
+sns_client = boto3.client('sns')
+volumes = ec2.describe_volumes()
+
+def lambda_handler(event, context):
+    unused_volumes = []
+    for vol in volumes['Volumes']:
+        if len(vol['Attachments']) == 0:
+            vol1 = ("-----Unused Volume ID = {}------".format(vol['VolumeId']))
+            unused_volumes.append(vol1)
+    
+    #email
+    sns_client.publish(
+        TopicArn='arn:aws:sns:us-east-1:045728217032:Notify-Unused-Volumes',
+        Subject='Warning - Unused Volume List',
+        Message=str(unused_volumes)
+    )
+    return "success"
