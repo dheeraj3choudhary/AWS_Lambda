7b62648108051a6ebdb2e6363d4342a259bb6e91
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2022-01-29 19:16:43 +0530

    Update read_s3_json.py

--- read_s3_json.py
+++ read_s3_json.py
@@ -1,18 +1,20 @@
-### Author:-Dheeraj Choudhary
 import boto3
 import json
+import ast
 s3_client = boto3.client('s3')
-dynamodb_client = boto3.client('dynamodb')
+dynamodb_client = boto3.resource('dynamodb')
 def lambda_handler(event, context):
     # First we will fetch bucket name from event json object
     bucket = event['Records'][0]['s3']['bucket']['name']
     # Now we will fetch file name which is uploaded in s3 bucket from event json object
-    file_key = ['Records'][0]['s3']['object']['key']
+    json_file_name = event['Records'][0]['s3']['object']['key']
     #Lets call get_object() function which Retrieves objects from Amazon S3 as dictonary
-    response = s3_client.get_object(Bucket='bucket', Key='file_key')
-    file_reader = response['Body'].read()
-    file_dic = json.loads(file_reader)
-    # As we have retrieved the dictionary from json object we will put it in dynamodb table
+    json_object = s3_client.get_object(Bucket=bucket,Key=json_file_name)
+    # Lets decode the json object returned by function which will retun string
+    file_reader = json_object['Body'].read().decode("utf-8")
+    # We will now change this json string to dictonary
+    file_reader = ast.literal_eval(file_reader)
+    # As we have retrieved the dictionary we will put it in dynamodb table
     table = dynamodb_client.Table('user')
-    table.put_item(Item=file_dic)
+    table.put_item(Item=file_reader)
     return 'success'
